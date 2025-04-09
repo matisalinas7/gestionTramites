45b075163afe21463a816a900ee9da7a84d43609
Matias Salinas <matisalinas.2000@gmail.com>
2025-04-09 11:14:55 -0300

    copiando proyecto de repo original

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+import utils.EjemplosPersistencia;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EjemplosPersistencia ejemplos = new EjemplosPersistencia();
+        ejemplos.crearElementos2();
+        
+        System.out.println("Funcionando");
+    }
+
+}
