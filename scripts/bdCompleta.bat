1620b45bb70272ce8cc4aad974f940b67705a9e6
Matias Salinas <matisalinas.2000@gmail.com>
2025-04-23 18:35:06 -0300

    bat con la bd

---
+++ scripts/bdCompleta.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root --password=disenio disenio -e "drop database disenio"
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root --password=disenio  mysql -e "create database disenio"
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root --password=disenio  disenio < FINAL.sql
